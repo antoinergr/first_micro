5436817056af0642d6c612cd7b1511b172c02073
antoinergr <antoine.roger14@gmail.com>
2022-12-04 18:56:55 +0100

    add googla analytic

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/', methods=["GET"])
 def hello_world():
-    return "J'adore les carbonara"
+ prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async
+    src="https://www.googletagmanager.com/gtag/js?id=UA-250988407-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', ' UA-250988407-1');
+    </script>
+    """
+ return prefix_google + "J'adore les carbonara"
