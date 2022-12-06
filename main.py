6d469ec1da8226931d2384f9064489ec09bef87d
antoinergr <antoine.roger14@gmail.com>
2022-12-06 15:45:41 +0100

    add logg browser

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-
+import logging
 from flask import Flask
+
+LOGGER = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
@@ -7,14 +9,18 @@ app = Flask(__name__)
 def hello_world():
     prefix_google = """
     <!-- Google tag (gtag.js) -->
-    <script async 
-    src="https://www.googletagmanager.com/gtag/js?id=UA-250924533-1"></script> 
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZFXZZQTC1P"></script>
     <script>
-    window.dataLayer = window.dataLayer || []; 
-    function gtag(){dataLayer.push(arguments);} 
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    
-    gtag('config', 'UA-250924533-1'); 
-    </script>
-    """
+
+    gtag('config', 'G-ZFXZZQTC1P');
+    </script>"""
     return prefix_google + "i love Carbonara"
+
+@app.route('/logger', methods=["GET"])
+def logger():
+    script = """
+    <script> console.log("logger") </script>"""
+    return "I'm a logger" + script
