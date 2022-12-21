4ccbcdc2c70a4c01ba4ff01fb7747d877c42ea5f
antoinergr <antoine.roger14@gmail.com>
2022-12-21 11:59:31 +0100

    add cookie

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import logging
-from flask import Flask
+from flask import Flask, render_template
+
+import requests
 
 LOGGER = logging.getLogger(__name__)
 
@@ -19,8 +21,22 @@ def hello_world():
     </script>"""
     return prefix_google + "i love Carbonara"
 
+
 @app.route('/logger', methods=["GET"])
 def logger():
-    script = """
-    <script> console.log("logger") </script>"""
-    return "I'm a logger" + script
+    app.logger.warning('testing ')
+    script="""
+     <script>console.log("Is that a log?")</script>
+    <input type="text" id="name" name="name" required placeholder = "hello Luiz">
+    """
+    return script
+
+@app.route('/cookie', methods=['GET', 'POST'])
+def cookie():
+    req=requests.get('https://www.google.com/')
+    return req.cookies.get_dict()
+
+@app.route('/cookie/analytics', methods=['GET', 'POST'])
+def cookie_analytics():
+    req=requests.get('https://analytics.google.com/analytics/web/#/p345039963/reports/intelligenthome')
+    return req.text
