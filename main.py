fabf041ed520616057564f4b65a9bb66d0941c27
antoinergr <antoine.roger14@gmail.com>
2023-01-05 20:55:21 +0100

    add trends route and html template

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import logging
 from flask import Flask, render_template
 
+
+from pytrends.request import TrendReq
+pytrends = TrendReq(hl='fr', tz=360)
+
 import requests
+import pandas as pd
+import numpy as np
 
 LOGGER = logging.getLogger(__name__)
 
@@ -40,3 +46,21 @@ def cookie():
 def cookie_analytics():
     req=requests.get('https://analytics.google.com/analytics/web/#/p345039963/reports/intelligenthome')
     return req.text
+
+
+@app.route('/trends',methods = ['GET', 'POST'])
+def trend():
+    kw=['Lionel Messi']
+    df=pytrends.get_historical_interest(kw, year_start=2022, 
+        month_start=10, day_start=1,  
+        year_end=2022, month_end=12, day_end=31, 
+        cat=0, geo='FR', gprop='', sleep=0).reset_index()
+    df['date']=pd.to_datetime(df['date'])
+    df=df.resample('D', on='date').mean()
+    df['Lionel Messi'] = df['Lionel Messi'].fillna(0)
+    line_labels = df.index.strftime("%m/%d/%Y").tolist()
+    line_values = df['Lionel Messi'].tolist()
+    return render_template('chart.html', labels=line_labels, values=line_values)
+
+if __name__ == "__main__":
+    app.run(debug=True)
